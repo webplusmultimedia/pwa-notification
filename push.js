a94871156502d50fd4f6d48c5e21f255cb33c72c
webplusm <contact@webplusm.net>
2019-11-17 11:34:26 -0400

    fix

--- push.js
+++ push.js
@@ -5,9 +5,13 @@ let vapiKeys = {
     privateKey: 'AVIPfXIk5YyCXoCi9hDdEOcggQFUStFpN3u3oeXV0zw'
 };
 
-push.setVapiDetails('mailto:test@webplusm.net',vapiKeys.publicKey,vapiKeys.privateKey);
-let sub={
-
+push.setVapidDetails('mailto:test@webplusm.net', vapiKeys.publicKey, vapiKeys.privateKey);
+let sub = {
+    endpoint: "https://fcm.googleapis.com/fcm/send/fviJFmXwWjU:APA91bFgfiTQ9OFhBCfAK3I2Pl1o_HOYha6kPoCVdqeGdy5GtYi7h6_Cf_IxvDZSPlYMKK4YkMq0GB3lmrtdvzRfrnLqeQ6Fi2Z1FhH5rXqxES0-cwnsbR5jzDGN2NhMz1oPEj2RSgUL",
+    expirationTime: null,
+    keys: {
+        p256dh: "BEseANoll16FPfVdDsW4vZ1xwDpSWJdOhclCHvNv6mnGIDl1tEnKZ5piUC1WfUaIG9c5m1TmyE6zjgg8ZEdoJ2U", auth: "OrEv4_PNqQRXol-NgiuoZA"
+    }
 };
 
-push.sendNotification(sub,'test message');
+push.sendNotification(sub, 'test message');
--- sw.js
+++ sw.js
@@ -1,3 +1,14 @@
-self.addEventListener('push',()=>{
-    self.registration.sendNotification('test message',{});
+self.addEventListener('push', function (e) {
+    var options = {
+        body: 'Here is a notification body!',
+        icon: 'images/example.png',
+        vibrate: [100, 50, 100],
+        data: {
+            dateOfArrival: Date.now(),
+            primaryKey: 1
+        }
+    };
+    e.waitUntil(
+        self.registration.sendNotification('test message', options)
+    )
 });
