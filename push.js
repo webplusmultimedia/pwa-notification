df2c7349e4ce72c444e3b9e247fbb66955fb4ddd
webplusm <contact@webplusm.net>
2019-11-17 14:14:42 -0400

    try some changes OneSignal

--- push.js
+++ push.js
@@ -6,7 +6,7 @@ let vapiKeys = {
 };
 
 push.setVapidDetails('mailto:test@webplusm.net', vapiKeys.publicKey, vapiKeys.privateKey);
-let sub = {
+let sub1 = {
     endpoint: "https://fcm.googleapis.com/fcm/send/fviJFmXwWjU:APA91bFgfiTQ9OFhBCfAK3I2Pl1o_HOYha6kPoCVdqeGdy5GtYi7h6_Cf_IxvDZSPlYMKK4YkMq0GB3lmrtdvzRfrnLqeQ6Fi2Z1FhH5rXqxES0-cwnsbR5jzDGN2NhMz1oPEj2RSgUL",
     expirationTime: null,
     keys: {
@@ -14,4 +14,13 @@ let sub = {
     }
 };
 
-push.sendNotification(sub, 'test message');
+let sub2 = {
+    "endpoint":"https://updates.push.services.mozilla.com/wpush/v2/gAAAAABd0Yfi8aTmOUWXspv0bA6W9TbCEPtxp7TpVkkXHiXnba08eLNHcBx97aM8yK6gWchTTA9mq0VXZiYnjpxC7PbBrQfb7E_-mHfsfkdix1xwS7VR4p5Nu_cmfpJoyEiLTsi-ly0VeCDX72J2KurF4Ykjh5yZhdi9iszRAqlakcfaZu0Fs2o",
+    "keys":{
+        "auth":"zdYM4S277k_TKARRD0tPbg",
+        "p256dh":"BDQp64C9uVj4ncfQyJrM4XL_hBWSsGx1paKaNeWRNAt_MB1oS9W_nJpEXVbT6t3raX5vYv6xMxb58l39X3Nuf8k"
+    }
+};
+
+push.sendNotification(sub1, 'test message');
+push.sendNotification(sub2, 'test message');
