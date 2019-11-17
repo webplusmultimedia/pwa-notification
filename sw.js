690ee929b1aae934121fc946af6883fb870651dd
webplusm <contact@webplusm.net>
2019-11-17 12:42:25 -0400

    fix some changes

--- sw.js
+++ sw.js
@@ -18,3 +18,16 @@ self.addEventListener('push', evt => {
         self.registration.showNotification('test message', options)
     )
 });
+
+self.addEventListener('notificationclick', function(e) {
+    var notification = e.notification;
+    var primaryKey = notification.data.primaryKey;
+    var action = e.action;
+
+    if (action === 'close') {
+        notification.close();
+    } else {
+        clients.openWindow('http://www.example.com');
+        notification.close();
+    }
+});
