e6b50f17d00be86db893cd90450b369c04b57f37
webplusm <contact@webplusm.net>
2019-11-18 09:56:42 -0400

    try some changes

--- push.js
+++ push.js
@@ -1,26 +1,24 @@
-var push = require('web-push');
+var push = require("web-push");
 
 let vapiKeys = {
-    publicKey: 'BEf1IWsWxpYHwvd9uKZPjCSKUtja__vryjavo1j8wqZopNhm70kWhzb6olPzKt6X196gQCsDeYMgVDXq-HY2-A4',
-    privateKey: 'AVIPfXIk5YyCXoCi9hDdEOcggQFUStFpN3u3oeXV0zw'
+    publicKey: "BEf1IWsWxpYHwvd9uKZPjCSKUtja__vryjavo1j8wqZopNhm70kWhzb6olPzKt6X196gQCsDeYMgVDXq-HY2-A4",
+    privateKey: "AVIPfXIk5YyCXoCi9hDdEOcggQFUStFpN3u3oeXV0zw"
 };
 
-push.setVapidDetails('mailto:test@webplusm.net', vapiKeys.publicKey, vapiKeys.privateKey);
+push.setVapidDetails("mailto:test@test.com", vapiKeys.publicKey, vapiKeys.privateKey);
 let sub1 = {
-    endpoint: "https://fcm.googleapis.com/fcm/send/fviJFmXwWjU:APA91bFgfiTQ9OFhBCfAK3I2Pl1o_HOYha6kPoCVdqeGdy5GtYi7h6_Cf_IxvDZSPlYMKK4YkMq0GB3lmrtdvzRfrnLqeQ6Fi2Z1FhH5rXqxES0-cwnsbR5jzDGN2NhMz1oPEj2RSgUL",
-    expirationTime: null,
-    keys: {
-        p256dh: "BEseANoll16FPfVdDsW4vZ1xwDpSWJdOhclCHvNv6mnGIDl1tEnKZ5piUC1WfUaIG9c5m1TmyE6zjgg8ZEdoJ2U", auth: "OrEv4_PNqQRXol-NgiuoZA"
-    }
+    "endpoint": "https://fcm.googleapis.com/fcm/send/d-qGAvUwdP4:APA91bH8C6yN3e-Sb4C6xjJBiMvbvPXNT1OSKV_LpmNxH4w63iX0YX01d8nUEPjIbjrgl-rRiufQdoLsk6NeYnyrN5RnMkhKj__vIH1HiCBOvG72luBwgak36hpnTwDZf-x-BXOLGae7",
+    "expirationTime": null,
+    "keys": {"p256dh": "BKZuj_uGZAWEqcKEb9KNaQHJwnDS1zwcAy24LyYsBtvb97kQiXKFlkJks2ovQ2FBZlO4vZ7gc8vfbBXID_50HEA", "auth": "r6-3rs5Mk0omU3wNutaDmg"}
 };
 
 let sub2 = {
-    "endpoint":"https://updates.push.services.mozilla.com/wpush/v2/gAAAAABd0Yfi8aTmOUWXspv0bA6W9TbCEPtxp7TpVkkXHiXnba08eLNHcBx97aM8yK6gWchTTA9mq0VXZiYnjpxC7PbBrQfb7E_-mHfsfkdix1xwS7VR4p5Nu_cmfpJoyEiLTsi-ly0VeCDX72J2KurF4Ykjh5yZhdi9iszRAqlakcfaZu0Fs2o",
-    "keys":{
-        "auth":"zdYM4S277k_TKARRD0tPbg",
-        "p256dh":"BDQp64C9uVj4ncfQyJrM4XL_hBWSsGx1paKaNeWRNAt_MB1oS9W_nJpEXVbT6t3raX5vYv6xMxb58l39X3Nuf8k"
+    "endpoint": "https://updates.push.services.mozilla.com/wpush/v2/gAAAAABd0Yfi8aTmOUWXspv0bA6W9TbCEPtxp7TpVkkXHiXnba08eLNHcBx97aM8yK6gWchTTA9mq0VXZiYnjpxC7PbBrQfb7E_-mHfsfkdix1xwS7VR4p5Nu_cmfpJoyEiLTsi-ly0VeCDX72J2KurF4Ykjh5yZhdi9iszRAqlakcfaZu0Fs2o",
+    "keys": {
+        "auth": "zdYM4S277k_TKARRD0tPbg",
+        "p256dh": "BDQp64C9uVj4ncfQyJrM4XL_hBWSsGx1paKaNeWRNAt_MB1oS9W_nJpEXVbT6t3raX5vYv6xMxb58l39X3Nuf8k"
     }
 };
 
-push.sendNotification(sub1, 'test message');
-push.sendNotification(sub2, 'test message');
+push.sendNotification(sub1, "test message").catch((err)=> console.log(err));
+push.sendNotification(sub2, "test message").catch((err)=> console.log(err));
